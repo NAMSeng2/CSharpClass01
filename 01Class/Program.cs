67c248b10a2b7366df808566ef09f26d2e8df579
2016038017@cbnu.ac.kr <2016038017@cbnu.ac.kr>
2021-10-20 21:54:17 +0900

    초기 커밋.

---
+++ 01Class/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// 내용을 묶어서 설명하는 것 class
+// 객체지향 -> 내가 표현하고 싶은걸 클래스로 만들어서
+// 그 클래스로 객체를 만드는 프로그래밍.
+
+using System;
+
+namespace _01Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
